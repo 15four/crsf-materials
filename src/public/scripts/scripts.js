b57ea9d420e50e4919edd50f23f2763186ea9856
Jon Ambas <ambasp@gmail.com>
2015-03-11 17:07:27 -0400

    add dropdown functions and styles

--- src/public/scripts/scripts.js
+++ src/public/scripts/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-	
+	$('.material_category').click(function(){
+		$(this).find('.material_category_list').toggleClass('material_category_list--show');
+		$(this).find('.material_category_header_icon').toggleClass('close');
+	}).find('.material_category_list').click(function(e) {
+  	return false;
+	});;
 });
