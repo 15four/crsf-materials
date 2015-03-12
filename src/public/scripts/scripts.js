fd1ae4d4a2e762f8a4067a7785fa48e4018f5dd4
Jon Ambas <ambasp@gmail.com>
2015-03-12 09:55:24 -0400

    remove unused assets

--- src/public/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.material_category').click(function(){
-		$(this).toggleClass('material_category--show');
-	}).find('.material_category_list').click(function(e) {
-  	return false;
-	});;
-});
