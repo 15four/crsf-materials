c170a0eec3bea30837532fb134fbdf089d620db3
Jon Ambas <ambasp@gmail.com>
2015-03-12 09:18:51 -0400

    consolidate dropdown show classes

--- src/public/scripts/scripts.js
+++ src/public/scripts/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('.material_category').click(function(){
-		$(this).find('.material_category_list').toggleClass('material_category_list--show');
-		$(this).find('.material_category_header_icon').toggleClass('close');
+		$(this).toggleClass('material_category--show');
 	}).find('.material_category_list').click(function(e) {
   	return false;
 	});;
